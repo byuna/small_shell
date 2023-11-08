1ed64f43c8352b84daff077c405d4effd4ca315d
abe <byuna@oregonstate.edu>
2023-11-07 16:23:54 -0800

    added EOF detection and will exit with 0 status

--- smallsh.c
+++ smallsh.c
@@ -44,8 +44,11 @@ int main(int argc, char *argv[])
 
     clearerr(input);
     errno = 0;
+
+    if (feof(input) != 0) {
+      break;
+    }
     ssize_t line_len = getline(&line, &n, input);       // Read getline man pages.
-    
     if (line_len < 0) err(1, "%s", input_fn);
    
     // number of words. wordsplit puts line into individual words into words array.
